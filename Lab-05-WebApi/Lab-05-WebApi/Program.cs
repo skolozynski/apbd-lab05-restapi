2857141f5d5cbeef0ef8696c102f3385f3d1df8b
Mateusz Skolozynski <mateusz.skolozynski15@gmail.com>
2024-04-09 12:57:28 +0200

    Exaample of MinimalAPI app

--- Lab-05-WebApi/Lab-05-WebApi/Program.cs
+++ Lab-05-WebApi/Lab-05-WebApi/Program.cs
@@ -16,29 +16,22 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+// MinimalAPI
+app.MapGet("/animals", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    return Results.Ok();
+});
+app.MapGet("/animals/{id:int}", (int id) =>
+{
+    return Results.Ok(id);
+});
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapPost("/animals", () =>
+{
+    return Results.Created();
+});
+
+
+//
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
